ef7e063394c493da8f8f9b4c43b039c8e39cbd88
abuz4r <52089495+abuz4r@users.noreply.github.com>
2021-12-30 19:57:10 +0000

    main

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Header.css";
+import unnamed from "../assets/header/unnamed.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={unnamed} className="logo" alt="" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} />
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, Item or User..."
+        />
+      </div>
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>MarketPlace</p>
+        <p>Create</p>
+      </div>
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
